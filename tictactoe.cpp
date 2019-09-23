768ecfc88e60f18fb56eb6355408fb063d1797bb
U-BSD\370801 <370801@SHS-10DECBC5A.beaverton.k12.or.us>
2019-09-23 09:12:07 -0700

    Add tictactoe.cpp

---
+++ tictactoe.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int** clearBoard(int grid[][]);
+
+int main() {
+	int board[3][3];
+	int x = 1;
+	int o = 2;
+	int blank = 0;
+	return 0;
+}
+
+int** clearBoard(int grid[][]) {
+	 
+}
