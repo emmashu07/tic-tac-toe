1ef90c132e31a4722aeaaf3d1a0f7cdba01c2327
U-BSD\370801 <370801@SHS-10DECBC5A.beaverton.k12.or.us>
2019-09-25 09:01:35 -0700

    tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -2,16 +2,24 @@
 
 using namespace std;
 
-int** clearBoard(int grid[][]);
+int** clearBoard(int grid[3][3]);
+
+int board[3][3];
+int x = 1;
+int o = 2;
+int blank = 0;
 
 int main() {
-	int board[3][3];
-	int x = 1;
-	int o = 2;
-	int blank = 0;
+	board = clearBoard(board);
+	
 	return 0;
 }
 
-int** clearBoard(int grid[][]) {
-	 
+int** clearBoard(int grid[3][3]) {
+	for(int i = 0; i < 3; i++) {
+	       for(int j = 0; j < 3; j++) {
+	       		grid[i][j] = blank;
+	       }
+	}
+	return grid;	
 }
