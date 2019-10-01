b26ed14ed5802852c14e8a9d6f6e3b7b33387ec3
U-BSD\370801 <370801@SHS-10DECBC5A.beaverton.k12.or.us>
2019-10-01 09:08:07 -0700

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -4,42 +4,66 @@ using namespace std;
 
 //int** clearBoard(int grid[3][3]);
 void printBoard(int grid[3][3]);
+void play(char in[2], int grid[3][3]);
 
 int board[3][3];
 int x = 1;
 int o = 2;
 int blank = 0;
+int player = 1;
+char input[2];
+bool xWin = false;
+bool oWin = false;
+bool tie = false;
 
 int main() {
 	printBoard(board);	
-	cout << "Welcome to Tic-Tac-Toe, enter a letter number followed by a letter to begin.";
-	
+	cout << "Welcome to Tic-Tac-Toe, enter a number, 1-3, followed by a letter, a-c, to begin." << endl;
+	while (!xWin && !oWin && !tie) {
+		cin >> input;
+		play(input, board);
+		printBoard(board);
+		//checkWin(player);
+		if (player == x) {
+			player = o;
+		}
+		else {
+			player = x;
+		}
+	}
 	return 0;
 }
 
 void printBoard(int grid[3][3]) {
 	cout << "\t1\t2\t3" << endl;
-	for(int i = 0; i < 3; i++) {
-		char str[10];
-		int counter = 0;
-		str[counter] = (char)('a' + i);
-		counter++;
+	for(int i = 0; i < 3; i++) {	
+		cout << (char)('a' + i);	
 		for(int j = 0; j < 3; j++) {
-			str[counter] = '\t';
-			counter++;
-			if(grid[i][j] == 1) {
-				str[counter] = 'X';
+			cout << '\t';	
+			if(grid[i][j] == x) {
+				cout << 'X';
 			}
-			else if(grid[i][j] == 2) {
-				str[counter] = 'O';
+			else if(grid[i][j] == o) {
+				cout << 'O';
 			}
 			else {
-				str[counter] = ' ';
+				cout << ' ';
 			}	
-			counter++;
 		}
-		cout << str << endl;
+		cout << endl;
 	}
+}
+
+void play(char in[2], int grid[3][3]) {
+	int first = in[0] - 1;
+	int second = (int)(in[1] - 'a');
+	if(player == x && grid[first][second] == blank) {	
+		grid[first][second] = x;
+	}
+	else if (player == o && grid[first][second] == blank) {
+		grid[first][second] = o;
+	}
+	printBoard(grid);
 }
 
 /*int** clearBoard(int grid[3][3]) {
